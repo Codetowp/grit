3a1815f38f379aab0fe5cc7b740cedc9e80538b5
sanjaykrishnadas <30548388+sanjaykrishnadas@users.noreply.github.com>
2017-08-16 17:31:06 +0530

    edit

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,6 +11,9 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function grit_customize_register( $wp_customize ) {
+    
+    require get_template_directory() . '/inc/customizer-controls.php';
+    
         $wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
         $wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
         $wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
@@ -37,16 +40,41 @@ function grit_customize_register( $wp_customize ) {
 
   if ( isset( $wp_customize->selective_refresh ) ) {
       
-        $wp_customize->selective_refresh->add_partial( 'grit_heder_text', array(
+        $wp_customize->selective_refresh->add_partial( 'grit_header_text', array(
                 'selector'        => '.container #head',
-                'render_callback' => 'grit_customize_partial_grit_heder_text',
+                'render_callback' => 'grit_customize_partial_grit_header_text',
             ) ); 
-      
-       $wp_customize->selective_refresh->add_partial( 'grit_header_description', array(
+
+        $wp_customize->selective_refresh->add_partial( 'grit_header_description', array(
                 'selector'        => '#home-banner h1',
                 'render_callback' => 'grit_customize_partial_grit_header_description',
             ) ); 
+
+        $wp_customize->selective_refresh->add_partial( 'grit_about_header', array(
+                'selector'        => '#about-us-block h2',
+                'render_callback' => 'grit_customize_partial_grit_about_header',
+            ) ); 
+
+        $wp_customize->selective_refresh->add_partial( 'grit_about_description', array(
+                'selector'        => '#about-us-block .section-title  p',
+                'render_callback' => 'grit_customize_partial_grit_about_description',
+            ) ); 
+
+        $wp_customize->selective_refresh->add_partial( 'grit_contact_header', array(
+                'selector'        => '#home-contact-block  p',
+                'render_callback' => 'grit_customize_partial_grit_contact_header',
+            ) ); 
       
+        $wp_customize->selective_refresh->add_partial( 'grit_work_header', array(
+                'selector'        => '#our-work-block .section-title  p',
+                'render_callback' => 'grit_customize_partial_grit_work_header',
+            ) ); 
+      
+        $wp_customize->selective_refresh->add_partial( 'grit_process_header', array(
+                'selector'        => '#process-block  h2',
+                'render_callback' => 'grit_customize_partial_grit_process_header',
+            ) ); 
+     
   }
     
 /********* header intro **********/
@@ -75,15 +103,15 @@ function grit_customize_register( $wp_customize ) {
             ) 
         ) );
 
-        $wp_customize->add_setting( 'grit_heder_text', array(      
+        $wp_customize->add_setting( 'grit_header_text', array(      
             'default'                   => 'Dcrazed says' ,
             'sanitize_callback'         => 'sanitize_text_field',
-            'transport'                 => 'postMessage', // refresh or postMessage              
+            'transport'                 => 'refresh', // refresh or postMessage              
         ) );    
 
-        $wp_customize->add_control( 'grit_heder_text', array(
+        $wp_customize->add_control( 'grit_header_text', array(
             'type'						=> 'text',
-            'label' 					=> __( 'Heading', 'grit' ),
+            'label' 					=> __( 'Header', 'grit' ),
             'section'  					=> 'grit_header',
             'priority' 					=> 2,
         ) );
@@ -96,7 +124,7 @@ function grit_customize_register( $wp_customize ) {
         ) );    
 
         $wp_customize->add_control( 'grit_header_description', array(
-            'type'						=> 'text',
+            'type'						=> 'textarea',
             'label' 					=> __( 'Description', 'grit' ),
             'section'  					=> 'grit_header',
             'priority' 					=> 3,
@@ -105,7 +133,7 @@ function grit_customize_register( $wp_customize ) {
 /********* about us **********/
         
         $wp_customize->add_section('grit_about_section', array(
-            'title'                     => __('About Section', 'grits'),
+            'title'                     => __('About Section', 'grit'),
 
             'priority'                  => 101,
 
@@ -121,7 +149,7 @@ function grit_customize_register( $wp_customize ) {
 
         $wp_customize->add_control( 'grit_about_header', array(
             'type'						=> 'text',
-            'label' 					=> __( 'Heading', 'grit' ),
+            'label' 					=> __( 'Header', 'grit' ),
             'section'  					=> 'grit_about_section',
             'priority' 					=> 2,
         ) );
@@ -134,7 +162,7 @@ function grit_customize_register( $wp_customize ) {
         ) );    
     
         $wp_customize->add_control( 'grit_about_description', array(
-            'type'						=> 'text',
+            'type'						=> 'textarea',
             'label' 					=> __( 'Description', 'grit' ),
             'section'  					=> 'grit_about_section',
             'priority' 					=> 3,
@@ -168,10 +196,175 @@ function grit_customize_register( $wp_customize ) {
             'priority' 					=> 5
         ) );	  
 
+/********* contact us **********/    
+      
+        $wp_customize->add_section('grit_contact_section', array(
+            'title'                     => __('Contact Section', 'grit'),
+            'priority'                  => 101,
+
+        ));
+
+
+        $wp_customize->add_setting( 'grit_contact_header', array(      
+            'default'                   => 'Increase leads, build a professional website and be awesome..' ,
+            'sanitize_callback'         => 'sanitize_text_field',
+            'transport'                 => 'refresh', // refresh or postMessage              
+        ) );    
+
+
+        $wp_customize->add_control( 'grit_contact_header', array(
+            'type'						=> 'text',
+            'label' 					=> __( 'Header', 'grit' ),
+            'section'  					=> 'grit_contact_section',
+            'priority' 					=> 2,
+        ) );
+
+        $wp_customize->add_setting( 'grit_contact_button_text', array(      
+            'default'                   => 'CONTACT US' ,
+            'sanitize_callback'         => 'sanitize_text_field',
+            'transport'                 => 'refresh',               
+        ) );    
+
+        $wp_customize->add_control( 'grit_contact_button_text', array(
+            'type'						=> 'text',
+            'label' 					=> __( 'Button Text', 'grit' ),
+            'section'  					=> 'grit_contact_section',
+            'priority' 					=> 3,
+        ) );	
+
+
+        $wp_customize->add_setting( 'grit_contact_button_url', array(      
+            'default'                   => 'www.burstfly.com' ,
+            'sanitize_callback'         => 'sanitize_text_field',
+            'transport'                 => 'refresh',               
+        ) );    
+
+        $wp_customize->add_control( 'grit_contact_button_url', array(
+            'type'						=> 'text',
+            'label' 					=> __( 'Button Url', 'grit' ),
+            'section'  					=> 'grit_contact_section',
+            'priority' 					=> 5
+        ) );	  
+
+/********* our work **********/    
+      
+        $wp_customize->add_section('grit_work_section', array(
+            'title'                     => __('Work Section', 'grit'),
+            'priority'                  => 104,
+
+        ));
+    
+        $wp_customize->add_setting( 'grit_work_header', array(      
+            'default'                   => 'Our work' ,
+            'sanitize_callback'         => 'sanitize_text_field',
+            'transport'                 => 'refresh', // refresh or postMessage              
+        ) );    
+
+
+        $wp_customize->add_control( 'grit_work_header', array(
+            'type'						=> 'text',
+            'label' 					=> __( 'Header', 'grit' ),
+            'section'  					=> 'grit_work_section',
+            'priority' 					=> 2,
+        ) );
+    
+
     
     
+
+/********* process block **********/    
+      
+        $wp_customize->add_section('grit_process_section', array(
+            'title'                     => __('Process Section', 'grit'),
+            'priority'                  => 104,
+
+        ));
     
+        $wp_customize->add_setting( 'grit_process_header', array(      
+            'default'                   => 'The process' ,
+            'sanitize_callback'         => 'sanitize_text_field',
+            'transport'                 => 'refresh', // refresh or postMessage              
+        ) );    
+
+
+        $wp_customize->add_control( 'grit_process_header', array(
+            'type'						=> 'text',
+            'label' 					=> __( 'Header', 'grit' ),
+            'section'  					=> 'grit_process_section',
+            'priority' 					=> 2,
+        ) );
     
+/********* Counter section **********/   
+
+        $wp_customize->add_section('grit_counter_section', array(
+            'title'                     => __('Counter Section', 'grit'),
+            'priority'                  => 105,
+
+        ));
+
+        $wp_customize->add_setting( 'grit_counter_setting', 
+               array(               
+                   'sanitize_callback' => 'grit_sanitize_repeatable_data_field',
+                    'transport' => 'refresh', // refresh or postMessage
+
+               ) );    
+
+
+        $wp_customize->add_control(
+                new Grit_Customize_Repeatable_Control(
+                    $wp_customize,
+                    'grit_counter_setting',
+                    array(
+                        'label'     => esc_html__('Counter Sections', 'grit'),
+                        'description'   => 'Add upto 4 service blocks',
+                        'section'       => 'grit_counter_section',
+                        //'live_title_id' => 'user_id', // apply for unput text and textarea only
+                        'title_format'  => esc_html__( '[live_title]', 'grit'), // [live_title]
+                        'max_item'      => 4, // Maximum item can add
+                        'limited_msg' 	=> wp_kses_post( 'Contact us through our Support Forum if you need more.', 'grit' ),
+                        'fields'    => array(
+
+                         'icon_type'  => array(
+                            'title' => esc_html__('Custom icon', 'grit'),
+                            'type'  =>'select',
+                            'options' => array(
+                                'icon' => esc_html__('Icon', 'grit'),
+                                'image' => esc_html__('image', 'grit'),
+                            ),
+                        ),
+
+                        'icon'  => array(
+                            'title' => esc_html__('Icon', 'grit'),
+                            'type'  =>'icon',
+                            'required' => array( 'icon_type', '=', 'icon' ),
+                        ),
+
+                        'image'  => array(
+                            'title' => esc_html__('Image', 'grit'),
+                            'type'  =>'media',
+                            'required' => array( 'icon_type', '=', 'image' ),
+                        ),
+
+                             'count' => array(
+                                'title' => esc_html__('Count', 'grit'),
+                                'type'  =>'text',
+                                'default' => wp_kses_post('455', 'grit'),
+                            ),
+
+
+                             'title' => array(
+                                'title' => esc_html__('Title', 'grit'),
+                                'type'  =>'text',
+                                'default' => wp_kses_post('CLIENTS'),
+                            ),
+                        ),
+
+                    )
+                )
+            );
+
+
+
 
 }
 add_action( 'customize_register', 'grit_customize_register' );
@@ -195,7 +388,27 @@ function grit_customize_partial_blogdescription() {
 }
 
 function grit_customize_partial_grit_header_description() {
-    bloginfo('grit_heder_text');
+    bloginfo('grit_header_text');
+}
+
+function grit_customize_partial_grit_about_header() {
+    bloginfo('grit_about_header');
+}
+
+function grit_customize_partial_grit_about_description() {
+    bloginfo('grit_about_description');
+}
+
+function grit_customize_partial_grit_contact_header() {
+    bloginfo('grit_contact_header');
+}
+
+function grit_customize_partial_grit_work_header() {
+    bloginfo('grit_work_header');
+}
+
+function grit_customize_partial_grit_process_header() {
+    bloginfo('grit_process_header');
 }
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
