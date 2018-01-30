64da06feda09a05b34cd5e168fd1342a6b07c3f4
Rijo Abraham <30547872+rijoabraham@users.noreply.github.com>
2018-01-30 12:17:56 +0530

    updates and min

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying search forms in xtremelysocial
+ * Our template for displaying custom search form
  *
  * @package grit
  */
@@ -11,7 +11,7 @@
 	<div class="input-group">
 		<input class="form-control" type="text"  placeholder="<?php echo esc_attr_x( 'Type Here&hellip;', 'placeholder', 'grit' ); ?>"  value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'grit' ); ?>">
 		<span class="input-group-btn">
-			<button  type="text" value="<?php echo esc_attr_x( 'Search', 'submit button', 'grit' ); ?>"><i class="fa  fa-search"></i></button>
+			<button  type="text" value="<?php echo esc_attr_x( 'Search', 'submit button', 'grit' ); ?>"><i class="fa fa-search"></i></button>
 		</span>
 	</div>
 </form>
