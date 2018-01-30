43d212837804f962c9601c0b0e635ad886718311
Rijo Abraham <30547872+rijoabraham@users.noreply.github.com>
2018-01-30 12:25:06 +0530

    completed

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
 get_header(); ?>
 <section class="page">
 	<div id="page-banner" style="background-image: url(<?php header_image(); ?>);">
-		<div class="content  wow fdeInUp">
+		<div class="content wow fdeInUp">
 			<div class="container ">
 				<header class="page-header">
 					<h1 class="page-title"> <?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'grit' ); ?> </h1>
@@ -20,24 +20,18 @@ get_header(); ?>
 	</div>
 	<div id="page-body">
 		<div class="container">
-			<div class="row  wow fdeInUp"> 
+			<div class="row wow fadeInUp"> 
 			<!--blog page container-->
-				<div class="col-md-9 col-sm-7 col-xs-12 page-block "   > 
-					<div id="primary" class="content-area">
-						<main id="main" class="site-main">
-							<section class="error-404 not-found">								
-								<div class="page-content">
-									<p>
-										<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'grit' ); ?>
-									</p>
-									<?php get_search_form();?>
-								</div><!-- .page-content -->
-							</section><!-- .error-404 -->
-						</main><!-- #main -->
-					</div><!-- #primary -->
+				<div class="col-md-9 col-sm-7 col-xs-12 page-block"> 							
+					<div class="page-content">
+						<p>
+							<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'grit' ); ?>
+						</p>
+						<?php get_search_form();?>
+					</div><!-- .page-content -->
 				</div>
-				<aside class="col-md-3 col-sm-5" > 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?> 
+				<aside class="col-md-3 col-sm-5"> 
+					<?php the_widget( 'Grit_WP_Widget_Recent_Posts' ); ?> 
 				</aside>
 			</div>
 		</div>
