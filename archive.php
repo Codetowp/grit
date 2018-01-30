b51009f84db9c650d5070ff067a31e498665cc75
Rijo Abraham <30547872+rijoabraham@users.noreply.github.com>
2018-01-30 12:37:19 +0530

    completed

--- archive.php
+++ archive.php
@@ -9,58 +9,48 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main">
-		<?php if ( have_posts() ) : ?>
-			<div id="page-banner" style="background-image: url(<?php header_image(); ?>);">
-				<div class="content  wow fdeInUp">
-					<div class="container ">
-						<header class="page-header">
+	<div id="page-banner" style="background-image: url(<?php header_image(); ?>);">
+		<div class="content wow fadeInUp">
+			<div class="container">
+				<header class="page-header">
+					<?php
+					the_archive_title( '<h1 >', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
+			</div>
+		</div>
+	</div>
+	<div id="page-body">
+		<div class="container">
+			<div class="row wow fadeInUp"> 
+				<!--blog page container-->
+				<div class="col-md-12 col-xs-12 page-block"> 
+					<?php if ( have_posts() ) : 
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();					
+						get_template_part( 'template-parts/content', 'archive' ); 						
+					endwhile;
+					?>
+					<div class="clearfix"></div>
+					<nav class="navigation posts-navigation wow fadeInUp" role="navigation">
+						<ul>
 							<?php
-							the_archive_title( '<h1 >', '</h1>' );
-							the_archive_description( '<div class="archive-description">', '</div>' );
+							the_posts_pagination( array(
+								'prev_text' => '<i class="fa fa-chevron-left"></i> ' . __( 'Newer posts', 'grit' ),
+								'next_text' => __( 'Older posts', 'grit' ) . ' <i class="fa fa-chevron-right"></i>',
+							) );
 							?>
-						</header><!-- .page-header -->
-					</div>
-				</div>
+							<?php wp_reset_postdata(); ?>
+						</ul>
+					</nav>
+					<div class="clearfix"></div>
+					<?php else :				
+					get_template_part( 'template-parts/content', 'none' );
+					endif; ?>
+				</div>				
 			</div>
-			<div id="page-body">
-				<div class="container">
-					<div class="row  wow fdeInUp"> 
-						<!--blog page container-->
-						<div class="col-md-12 col-xs-12 page-block "> 
-							<?php
-							/* Start the Loop */
-							while ( have_posts() ) : the_post();
-								?>
-								<!-- article-->
-								<?php 	get_template_part( 'template-parts/content', 'archive' ); ?>
-								<!--/ article --> 
-							<?php endwhile;?>
-
-							<div class="clearfix"></div>
-
-							<!--/portfolio page nav-->
-							<nav class="navigation posts-navigation  wow fadeInUp"  role="navigation">
-								<ul>
-									<?php
-									the_posts_pagination( array(
-										'prev_text' => '<i class="fa fa-chevron-left"></i> ' . __( 'Newer posts', 'grit' ),
-										'next_text' => __( 'Older posts', 'grit' ) . ' <i class="fa fa-chevron-right"></i>',
-									) );
-									?>
-									<?php wp_reset_postdata(); ?>
-								</ul>
-							</nav>
-						</div>
-						<!--blog page container-->
-						<div class="clearfix"></div>
-					</div>
-				</div>
-			</div>
-		<?php endif;?>
-	</main>
-</div>
+		</div>
+	</div>
 <?php
-//get_sidebar();
 get_footer();
